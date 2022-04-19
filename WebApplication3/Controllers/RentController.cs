b6e5296e51d002be7d241e9e238f9a8f8f84ed67
Elvis <ElvisGarcia20032020@gmail.com>
2022-04-19 02:01:26 -0400

    Update busqueda de vehiculo Add metodo Rentar incompleto

---
+++ WebApplication3/Controllers/RentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class RentController : Controller
+    {
+        private readonly RentAcarEntities1 db = new RentAcarEntities1();
+        public ActionResult RentView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Rentar(string idAuto, string idCliente, string fechaRenta, string precioRenta)
+        {
+            int idVehiculo = int.Parse(idAuto);
+            int idC = int.Parse(idCliente);
+            DateTime fecha = DateTime.Parse(fechaRenta);
+            double precio = double.Parse(precioRenta);
+
+            //Rentaes renta = new Rentaes(
+            //{
+            //    idVehiculo = idVehiculo,
+            //    idCliente = idC,
+            //    fecha = fecha,
+            //    precio = precio
+            //});
+
+            //db.Rentaes.Add(renta);
+
+            var vehiculo = db.Vehiculoes.Find(idVehiculo);
+            Vehiculo vehiculoModificado = new Vehiculo(){ Estado = "Inactivo"};
+            db.Entry(vehiculo).CurrentValues.SetValues(vehiculoModificado);
+
+            db.SaveChanges();
+        }
+    }
+}
